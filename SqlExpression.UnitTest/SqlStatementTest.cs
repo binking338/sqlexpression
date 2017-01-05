cefdb5e9b69981c18bce84d4281c351dd674fc7e
binking338 <binking338@qq.com>
2017-01-05 19:22:53 +0800

    增加单元测试用例

--- SqlExpression.UnitTest/SqlStatementTest.cs
+++ SqlExpression.UnitTest/SqlStatementTest.cs
@@ -13,11 +13,27 @@ namespace SqlExpression.UnitTest
         {
             ISelectStatement statement;
             statement = TestSchema.Select((sql, s) => { return sql.Get(s.All()); });
-            Assert.AreEqual("SELECT test.oid,test.oname,test.age,test.isdel FROM test", statement.ToString());
+            Assert.AreEqual("SELECT test.oid,test.oname,test.age,test.gender,test.isdel FROM test", statement.ToString());
 
             statement = TestSchema.Select((sql, s) => { return sql.Get(s.oid).Where(s.oid > 1); });
             Assert.AreEqual("SELECT test.oid FROM test WHERE test.oid>1", statement.ToString());
-            
+
+            statement = TestSchema.Select((sql, s) => { return sql.Get(s.age).OrderBy(s.age.Desc()); });
+            Assert.AreEqual("SELECT test.age FROM test ORDER BY test.age DESC", statement.ToString());
+
+            statement = TestSchema.Select((sql, s) => { return sql.Get(s.gender, s.age.Avg()).GroupBy(s.gender).Having(s.age.Avg() < 40); });
+            Assert.AreEqual("SELECT test.gender,AVG(test.age) FROM test GROUP BY test.gender HAVING AVG(test.age)<40", statement.ToString());
+
+            statement = TestSchema.Select((sql, s) => { return sql.Get(s.All()).Where(s.oid > 1).OrderBy(s.age.Desc()); });
+            Assert.AreEqual("SELECT test.oid,test.oname,test.age,test.gender,test.isdel FROM test WHERE test.oid>1 ORDER BY test.age DESC", statement.ToString());
+
+            statement = TestSchema.Select((sql, s) => { return sql.Get(s.age.Avg()).Where(s.oid > 1).GroupBy(s.gender).Having(s.age.Avg() > 18).OrderBy(s.age.Desc()); });
+            Assert.AreEqual("SELECT AVG(test.age) FROM test WHERE test.oid>1 GROUP BY test.gender HAVING AVG(test.age)>18 ORDER BY test.age DESC", statement.ToString());
+        }
+
+        public void Join()
+        {
+
         }
     }
 }
--- SqlExpression.UnitTest/TestSchema.cs
+++ SqlExpression.UnitTest/TestSchema.cs
@@ -12,6 +12,7 @@ namespace SqlExpression.UnitTest
         private static PropertyExpression _oid;
         private static PropertyExpression _oname;
         private static PropertyExpression _age;
+        private static PropertyExpression _gender;
         private static PropertyExpression _isdel;
 
         static TestSchema()
@@ -20,14 +21,82 @@ namespace SqlExpression.UnitTest
             _oid = new PropertyExpression("oid") { Type = DBType.MySql, Table = Table };
             _oname = new PropertyExpression("oname") { Type = DBType.MySql, Table = Table };
             _age = new PropertyExpression("age") { Type = DBType.MySql, Table = Table };
+            _gender = new PropertyExpression("gender") { Type = DBType.MySql, Table = Table };
             _isdel = new PropertyExpression("isdel") { Type = DBType.MySql, Table = Table };
             __PKs = new PropertyExpression[] { _oid };
-            __All = new PropertyExpression[] { _oid, _oname, _age, _isdel };
+            __All = new PropertyExpression[] { _oid, _oname, _age, _gender, _isdel };
         }
 
         public PropertyExpression oid { get { return _oid; } }
         public PropertyExpression oname { get { return _oname; } }
         public PropertyExpression age { get { return _age; } }
+        public PropertyExpression gender { get { return _gender; } }
+        public PropertyExpression isdel { get { return _isdel; } }
+
+        public PropertyExpression[] PKs()
+        {
+            return __PKs;
+        }
+
+        public PropertyExpression[] All()
+        {
+            return __All;
+        }
+    }
+
+    public class FooSchema : SchemaBase<FooSchema>
+    {
+        private static PropertyExpression[] __PKs;
+        private static PropertyExpression[] __All;
+        private static PropertyExpression _oid;
+        private static PropertyExpression _oname;
+        private static PropertyExpression _isdel;
+
+        static FooSchema()
+        {
+            Table = new TableExpression("foo");
+            _oid = new PropertyExpression("oid") { Type = DBType.MySql, Table = Table };
+            _oname = new PropertyExpression("oname") { Type = DBType.MySql, Table = Table };
+            _isdel = new PropertyExpression("isdel") { Type = DBType.MySql, Table = Table };
+            __PKs = new PropertyExpression[] { _oid };
+            __All = new PropertyExpression[] { _oid, _oname, _isdel };
+        }
+
+        public PropertyExpression oid { get { return _oid; } }
+        public PropertyExpression oname { get { return _oname; } }
+        public PropertyExpression isdel { get { return _isdel; } }
+
+        public PropertyExpression[] PKs()
+        {
+            return __PKs;
+        }
+
+        public PropertyExpression[] All()
+        {
+            return __All;
+        }
+    }
+
+    public class BarSchema : SchemaBase<BarSchema>
+    {
+        private static PropertyExpression[] __PKs;
+        private static PropertyExpression[] __All;
+        private static PropertyExpression _oid;
+        private static PropertyExpression _oname;
+        private static PropertyExpression _isdel;
+
+        static BarSchema()
+        {
+            Table = new TableExpression("foo");
+            _oid = new PropertyExpression("oid") { Type = DBType.MySql, Table = Table };
+            _oname = new PropertyExpression("oname") { Type = DBType.MySql, Table = Table };
+            _isdel = new PropertyExpression("isdel") { Type = DBType.MySql, Table = Table };
+            __PKs = new PropertyExpression[] { _oid };
+            __All = new PropertyExpression[] { _oid, _oname, _isdel };
+        }
+
+        public PropertyExpression oid { get { return _oid; } }
+        public PropertyExpression oname { get { return _oname; } }
         public PropertyExpression isdel { get { return _isdel; } }
 
         public PropertyExpression[] PKs()
