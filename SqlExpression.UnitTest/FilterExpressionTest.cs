754d3f7290a848d3692d8f1259208077564be6f8
binking338 <binking338@qq.com>
2017-01-09 10:21:21 +0800

    调整单元测试代码文件分布

---
+++ SqlExpression.UnitTest/FilterExpressionTest.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SqlExpression;
+using SqlExpression.MySql;
+
+namespace SqlExpression.UnitTest
+{
+    [TestClass]
+    public class FilterExpressionTest
+    {
+        #region Filter
+
+        /// <summary>
+        /// 普通查询条件
+        /// </summary>
+        [TestMethod]
+        public void CommonFilter()
+        {
+            var t = TestSchema.Instance;
+            IExpression e;
+
+            var oid = 1000;
+            e = t.oid.Eq(oid);
+            Assert.AreEqual(e.Expression, "test.oid=" + oid);
+
+            e = t.oid.Gt(oid);
+            Assert.AreEqual(e.Expression, "test.oid>" + oid);
+
+            e = t.oid.Lt(oid);
+            Assert.AreEqual(e.Expression, "test.oid<" + oid);
+
+            e = t.oid.GtOrEq(oid);
+            Assert.AreEqual(e.Expression, "test.oid>=" + oid);
+
+            e = t.oid.LtOrEq(oid);
+            Assert.AreEqual(e.Expression, "test.oid<=" + oid);
+
+
+            e = t.oid == oid;
+            Assert.AreEqual(e.Expression, "test.oid=" + oid);
+
+            e = t.oid > oid;
+            Assert.AreEqual(e.Expression, "test.oid>" + oid);
+
+            e = t.oid < oid;
+            Assert.AreEqual(e.Expression, "test.oid<" + oid);
+
+            e = t.oid >= oid;
+            Assert.AreEqual(e.Expression, "test.oid>=" + oid);
+
+            e = t.oid <= oid;
+            Assert.AreEqual(e.Expression, "test.oid<=" + oid);
+
+
+            e = t.oname.Like("%");
+            Assert.AreEqual(e.Expression, "test.oname LIKE '%'");
+
+            e = t.oname.NotLike("%");
+            Assert.AreEqual(e.Expression, "test.oname NOT LIKE '%'");
+
+            e = t.oid.Between(100, 1000);
+            Assert.AreEqual(e.Expression, "test.oid BETWEEN 100 AND 1000");
+
+            e = t.oid.NotBetween(100, 1000);
+            Assert.AreEqual(e.Expression, "test.oid NOT BETWEEN 100 AND 1000");
+
+            e = t.oid.In(1, 2, 3, 4);
+            Assert.AreEqual(e.Expression, "test.oid IN (1,2,3,4)");
+
+            e = t.oid.NotIn(1, 2, 3, 4);
+            Assert.AreEqual(e.Expression, "test.oid NOT IN (1,2,3,4)");
+
+            e = t.oid.IsNull();
+            Assert.AreEqual(e.Expression, "test.oid IS NULL");
+
+            e = t.oid.IsNotNull();
+            Assert.AreEqual(e.Expression, "test.oid IS NOT NULL");
+        }
+
+        /// <summary>
+        /// 普通查询条件 参数化查询
+        /// </summary>
+        [TestMethod]
+        public void CommonFilterVarParam()
+        {
+            var t = TestSchema.Instance;
+            IExpression e;
+
+            e = t.oid.EqVarParam();
+            Assert.AreEqual(e.Expression, "test.oid=@oid");
+
+            e = t.oid.GtVarParam();
+            Assert.AreEqual(e.Expression, "test.oid>@oid");
+
+            e = t.oid.LtVarParam();
+            Assert.AreEqual(e.Expression, "test.oid<@oid");
+
+            e = t.oid.GtOrEqVarParam();
+            Assert.AreEqual(e.Expression, "test.oid>=@oid");
+
+            e = t.oid.LtOrEqVarParam();
+            Assert.AreEqual(e.Expression, "test.oid<=@oid");
+
+            e = t.oname.LikeVarParam();
+            Assert.AreEqual(e.Expression, "test.oname LIKE @oname");
+
+            e = t.oname.NotLikeVarParam();
+            Assert.AreEqual(e.Expression, "test.oname NOT LIKE @oname");
+
+            e = t.oid.BetweenVarParam();
+            Assert.AreEqual(e.Expression, "test.oid BETWEEN @oid1 AND @oid2");
+
+            e = t.oid.NotBetweenVarParam();
+            Assert.AreEqual(e.Expression, "test.oid NOT BETWEEN @oid1 AND @oid2");
+        }
+
+        [TestMethod]
+        public void LogicFilter()
+        {
+            var t = TestSchema.Instance;
+            IExpression e;
+
+            e = (t.oid > 1) & (t.oid < 100);
+            Assert.AreEqual(e.Expression, "test.oid>1 AND test.oid<100");
+
+            e = (t.oid > 1).And(t.oid < 100);
+            Assert.AreEqual(e.Expression, "test.oid>1 AND test.oid<100");
+
+            e = (t.oid > 1) | (t.oid < 100);
+            Assert.AreEqual(e.Expression, "test.oid>1 OR test.oid<100");
+
+            e = (t.oid > 1).Or(t.oid < 100);
+            Assert.AreEqual(e.Expression, "test.oid>1 OR test.oid<100");
+        }
+
+        #endregion
+
+    }
+}
--- SqlExpression.UnitTest/SqlStatementTest.cs
+++ SqlExpression.UnitTest/SqlStatementTest.cs
@@ -13,51 +13,6 @@ namespace SqlExpression.UnitTest
     {
         ISqlStatement statement;
 
-        #region Filter
-
-        [TestMethod]
-        public void Filter()
-        {
-
-        }
-
-        #endregion
-
-        #region Params
-
-        [TestMethod]
-        public void Params()
-        {
-            statement = FooSchema.Insert((sql, s) => sql.Set(s.oid, 1).Set(s.oname, "foo1"));
-            Assert.AreEqual(SortedJoin(statement.Params), "");
-            statement = FooSchema.Table.Insert().SetP(FooSchema.Instance.oid).SetVarParam(FooSchema.Instance.oname);
-            Assert.AreEqual(SortedJoin(statement.Params), "oid,oname");
-            statement = FooSchema.Table.Insert().SetP(FooSchema.Instance.oid).SetVarParam(FooSchema.Instance.oname);
-            Assert.AreEqual(SortedJoin(statement.Params), "oid,oname");
-
-            statement = FooSchema.Delete((sql, s) => sql);
-            Assert.AreEqual(SortedJoin(statement.Params), "");
-            statement = FooSchema.Delete((sql, s) => sql.Where(s.oid.EqVarParam()));
-            Assert.AreEqual(SortedJoin(statement.Params), "oid");
-
-            statement = FooSchema.Update((sql, s) => sql.Set(s.oname, "foo2"));
-            Assert.AreEqual(SortedJoin(statement.Params), "");
-            statement = FooSchema.Table.Update().SetVarParam(FooSchema.Instance.oname).SetP(FooSchema.Instance.isdel).Where(FooSchema.Instance.oid.EqVarParam());
-            Assert.AreEqual(SortedJoin(statement.Params), "isdel,oid,oname");
-            
-            statement = TestSchema.Select((sql, s) => sql.Get(s.age.Avg()).Where(s.oid.GtVarParam()).GroupBy(s.gender).Having(18 < s.age.Avg()).OrderBy(s.age.Desc()));
-            Assert.AreEqual(SortedJoin(statement.Params), "oid");
-        }
-
-        private string SortedJoin(IEnumerable<string> strs, string splitter = ",")
-        {
-            var list = strs.ToList();
-            list.Sort();
-            return list.Join(splitter);
-        }
-
-        #endregion
-
         #region Select
 
         [TestMethod]
